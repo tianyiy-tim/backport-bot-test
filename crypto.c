5d8a4a1cd6a5cca22bd31a8944daef3d2e1b9a3e
tianyiy-tim <tianyiy@amazon.com>
2026-06-05 14:58:20 -0700

    fix: bounds check in process_handshake (cve-handshake-1)

--- crypto.c
+++ crypto.c
@@ -2,7 +2,11 @@
 
 #define MAX_HANDSHAKE 4096
 
+// Fixed: bounds check
 void process_handshake(char *buf, const char *input, int len) {
+    if (len > MAX_HANDSHAKE) {
+        return;
+    }
     memcpy(buf, input, len);
 }
 
