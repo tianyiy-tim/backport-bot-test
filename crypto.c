8bb77d0b08288fa4e904f6200318d920b79c4aea
tianyiy-tim <tianyiy@amazon.com>
2026-06-05 14:58:21 -0700

    NetOS: add debug logging to handshake

--- crypto.c
+++ crypto.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
 #include <string.h>
 
 #define MAX_HANDSHAKE 4096
 
 void process_handshake(char *buf, const char *input, int len) {
+    fprintf(stderr, "[NetOS] handshake len=%d\n", len);
     memcpy(buf, input, len);
 }
 
