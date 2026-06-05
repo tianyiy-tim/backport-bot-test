7aea3410563bce4a829008c3d148a3b561d69dc4
tianyiy-tim <tianyiy@amazon.com>
2026-06-05 15:48:05 -0700

    fix: constant-time hash_compare (cve-pure-modification)

--- crypto/digest.c
+++ crypto/digest.c
@@ -5,5 +5,9 @@ void compute_hash(char *out, const char *input, int len) {
 }
 
 int hash_compare(const char *a, const char *b) {
-    return memcmp(a, b, 32);
+    int diff = 0;
+    for (int i = 0; i < 32; i++) {
+        diff |= a[i] ^ b[i];
+    }
+    return diff;
 }
