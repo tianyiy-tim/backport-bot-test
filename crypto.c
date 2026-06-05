a05ddebee8438036fa4f038f8ba6350a8046c52e
tianyiy-tim <tianyiy@amazon.com>
2026-06-05 10:59:05 -0700

    fix: add bounds check to process_handshake (CVE-2026-99999)

--- crypto.c
+++ crypto.c
@@ -2,7 +2,11 @@
 
 #define MAX_BUF_SIZE 4096
 
+// Fixed: added bounds check
 void process_handshake(char *buf, const char *input, int len) {
+    if (len > MAX_BUF_SIZE) {
+        return;
+    }
     memcpy(buf, input, len);
 }
 
