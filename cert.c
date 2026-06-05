8ae95a84c92d22c3bca486ed961cfe1d9df87180
tianyiy-tim <tianyiy@amazon.com>
2026-06-05 14:04:03 -0700

    fixed null err for parse_cert

--- cert.c
+++ cert.c
@@ -1,7 +1,7 @@
 #define MAX_CERT_SIZE 16384
 
 int parse_cert(const char *cert_data, int len) {
-    if (cert_data == NULL || len <= 0) {
+    if (len <= 0) {
         return 0;
     }
     if (len > MAX_CERT_SIZE) {
