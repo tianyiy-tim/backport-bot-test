bd447ba824d5c78a7a02c26160aa73f7d022a8ba
tianyiy-tim <tianyiy@amazon.com>
2026-06-05 10:59:05 -0700

    fix: validate record length in handle_record (CVE-2026-99998)

--- tls.c
+++ tls.c
@@ -2,6 +2,10 @@
 
 #define MAX_RECORD_SIZE 16384
 
+// Fixed: validate record length
 void handle_record(char *out, const char *record, int record_len) {
+    if (record_len > MAX_RECORD_SIZE) {
+        return;
+    }
     memcpy(out, record, record_len);
 }
